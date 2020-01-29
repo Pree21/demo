d54fb90d81eabba13572723bcc2e97ec4247f3cf
preeti2112 <pree.php.30@gmail.com>
2020-01-29 09:06:03 +0530

    Updates

---
+++ precache-manifest.106ce6375da7400f1e9ca4954c3713e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13bcad89bcbd00cec577a03ff0495f6",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "4f9133d84ce110bf41d2",
+    "url": "/demo/static/css/main.5f365d1d.chunk.css"
+  },
+  {
+    "revision": "3263e3e94f13faf5105c",
+    "url": "/demo/static/js/2.3a6454a9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo/static/js/2.3a6454a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f9133d84ce110bf41d2",
+    "url": "/demo/static/js/main.479c07ab.chunk.js"
+  },
+  {
+    "revision": "94978c25055cbc935efb",
+    "url": "/demo/static/js/runtime-main.4b0efd09.js"
+  }
+]);
